a12fd23cf8aa3de4a81de9ab14d94963169421cf
AndriiLetskaliuk <andrewletskalyuk@gmail.com>
2022-11-04 13:24:21 +0200

    second

--- SalesRep_IntegrationTests/Controllers/BotServiceTest.cs
+++ SalesRep_IntegrationTests/Controllers/BotServiceTest.cs
@@ -21,9 +21,9 @@ namespace SalesRepUnitTests.Services
         private readonly Mock<ITradeOrderRepository> _repository;
         private readonly IBotService _botService;
         private readonly IHttpClientWrapper _httpClientWrapper;
-        private const string DEFAULT_STRING = "DEFAULT_STRING";
-        private const int DEFAULT_INT = 100;
-        private const int DEFAULT_ID = 1;
+        private const string DefaultString = "DEFAULT_STRING";
+        private const int DefaultInt = 100;
+        private const int DefaultId = 1;
         public BotServiceTest()
         {
             _httpClientWrapper = new HttpClientWrapper();
@@ -31,17 +31,17 @@ namespace SalesRepUnitTests.Services
             _botService = new BotService(_httpClientWrapper);
         }
         [Fact]
-        public async Task GetTradeOrdersByCustomerIDAsyncTest_ReturtListAsync()
+        public async Task GetTradeOrdersByCustomerIDAsyncTest_ReturnListAsync()
         {
             //Arrange
             var tradeOrder = new TradeOrder()
             {
                 CreatedDate = DateTime.Now,
                 DeliveryDate = DateTime.Now,
-                DeliveryAddress = DEFAULT_STRING,
-                SumOfOrder = DEFAULT_INT,
-                AdditionalInfo = DEFAULT_STRING,
-                CustomerID = DEFAULT_ID,
+                DeliveryAddress = DefaultString,
+                SumOfOrder = DefaultInt,
+                AdditionalInfo = DefaultString,
+                CustomerID = DefaultId,
                 Products = new List<Product>()
             };
             var saveTradeOrder = _repository.Setup(x => x.CreateOrder(It.IsAny<TradeOrder>())).ReturnsAsync(new OperationStatus() { IsSuccess = true });
@@ -57,7 +57,7 @@ namespace SalesRepUnitTests.Services
             string host = "https://localhost:5001/";
 
             // Act
-            var result = await _botService.GetListTradeOrders(DEFAULT_ID, host);
+            var result = await _botService.GetListTradeOrders(DefaultId, host);
 
             // Assert
             Assert.NotNull(result);
